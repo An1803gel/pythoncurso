41476b4b5b18f6389d168495448c0d2e419a2957
An1803gel <93747873+An1803gel@users.noreply.github.com>
2021-11-05 23:06:52 -0600

    Add files via upload

---
+++ Ejercicio2.py
@@ -0,0 +1,10 @@
+#Declarar variables
+radio = float(input("Escribe el radio: "))
+
+#Proceso
+area = 3.1416 * radio ** 2
+perimetro = 2 * 3.1416 * radio
+
+#Resultados 
+print("Area= ", round(area, 2))
+print("Perimetro= ", round(perimetro, 2))
