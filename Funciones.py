0adff91bd7bf6ca0b74dd2ac1af52020652e21bc
An1803gel <93747873+An1803gel@users.noreply.github.com>
2021-11-10 18:44:36 -0600

    Add files via upload

---
+++ Funciones.py
@@ -0,0 +1,43 @@
+#Funcion / Rutina: Tarea a ejecutar
+#Sintaxis: Nombre_Funcion (Argumentos o parametros)
+
+#Definir una funcion para saludar recibiendo un parametro
+def Saludar1(saludo):
+    return saludo     #Retornar / Devolver / Regresar un valor 
+
+def Saludar2(saludo):
+    print(saludo)
+
+def Sumar(num1, num2):
+    suma = num1 + num2
+    print(suma)
+
+def Resta(num1, num2):
+    resta = num1 - num2
+    print(resta)
+
+def Multiplicacion(num1, num2):
+    multiplicacion = num1 * num2
+    print(multiplicacion)
+
+def Division(num1, num2):
+    division = num1 / num2
+    print(division)
+
+saludo_cordial = "hola angel"
+
+# Mandar a llamar la funcion saludar 
+print(Saludar1(saludo_cordial))
+Saludar2(saludo_cordial)
+
+Sumar (10, 5)
+Sumar (20, 30)
+
+Resta (10, 5)
+Resta (20, 30)
+
+Multiplicacion (10, 5)
+Multiplicacion (20, 30)
+
+Division (10, 5)
+Division (10, 5)(20, 30)
