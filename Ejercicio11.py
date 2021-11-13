523222b6f2c68e9902b92d94fc6182686a6d5e0c
An1803gel <93747873+An1803gel@users.noreply.github.com>
2021-11-12 18:19:17 -0600

    Add files via upload

---
+++ Ejercicio11.py
@@ -0,0 +1,10 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #Numeros
+
+def Recorrer_arreglo (arreglo):
+    for elemento in arreglo:    
+      print(elemento)
+
+#for elemento in numeros:    
+    #print(elemento)
+
+Recorrer_arreglo (numeros)
