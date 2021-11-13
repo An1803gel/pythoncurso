1b00b4ca48f43d1edefa2f1955321697f9a45648
An1803gel <93747873+An1803gel@users.noreply.github.com>
2021-11-12 19:56:06 -0600

    Add files via upload

---
+++ Ejercicio13.py
@@ -0,0 +1,23 @@
+arreglo = [
+    { "Estado" : "Aguascalientes",
+       "Poblacion" : 1243465 ,
+       "Extension territorial": 8239   
+     },
+      { "Estado" : "Jalisco",
+       "Poblacion" : 1243465 ,
+       "Extension territorial": 4398 
+     },
+      { "Estado" : "Michoacan",
+       "Poblacion" : 1246465 ,
+       "Extension territorial": 1923
+     },
+      { "Estado" : "Guanajuato",
+       "Poblacion" : 1243465 ,
+       "Extension territorial": 8192
+     },
+      { "Estado" : "Veracruz",
+       "Poblacion" : 1243465 ,
+       "Extension territorial": 3874 
+     },
+]
+print (arreglo[0]["Estado"])
